cd510e5b4d2da3df033105ebdc413694290ce52d
heyong1 <2976832917@qq.com>
2022-05-18 15:39:59 +0800

    项目初始结构创建

---
+++ gulimail-coupon/src/test/java/com/heyong/gulimailcoupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyong.gulimailcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
